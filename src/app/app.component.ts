7e824fb38d07c1653cf8a9abcc1de1892214fc33
Jin Hu <jin.hu@inferencesolutions.com>
2018-09-19 17:02:44 +1000

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  cuisines: Observable<any[]>;
+  cuisinesRef: AngularFireList<any[]>;
+  restaurants: Observable<any[]>;
+  restaurantsRef: AngularFireList<any>;
+
+  constructor (private db: AngularFireDatabase) {
+  }
+
+  ngOnInit () {
+    this.cuisinesRef = this.db.list('/cuisines');
+    this.cuisines = this.cuisinesRef.valueChanges();
+
+    this.restaurantsRef = this.db.list('/restaurants');
+    this.restaurants = this.restaurantsRef.valueChanges().pipe(
+      map(restaurants => {
+        restaurants.map(restaurant => {
+          restaurant.cuisineType = this.db.object('/cuisines/' + restaurant.cuisine).valueChanges();
+          restaurant.featureTypes = [];
+          for (var f in restaurant.features) {
+            restaurant.featureTypes.push(this.db.object('/features/' + f).valueChanges());
+          }
+        });
+        return restaurants;
+      })
+    );
+  }
+
+  // add () {
+  //   this.cuisinesRef.push({
+  //     name: 'Asian',
+  //     details: {
+  //       description: 'bla'
+  //     }
+  //   });
+  // }
+  //
+  // update () {
+  //   this.afDb.object('/favourites/1/').set(null);
+  // }
+  //
+  // remove () {
+  //   this.afDb.object('/restaurants').remove()
+  //     .then(() => {
+  //       console.log('SUCCESS');
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCxBEX0rLe740y4sMVgin0swCfzGU9RiPc",
+    authDomain: "pepper-6ba78.firebaseapp.com",
+    databaseURL: "https://pepper-6ba78.firebaseio.com",
+    projectId: "pepper-6ba78",
+    storageBucket: "pepper-6ba78.appspot.com",
+    messagingSenderId: "67955095165"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
